801208711910def1715ba3de770b5c0e53eee47c
fahdfady <55513802+fahdfady@users.noreply.github.com>
2022-01-06 00:18:40 +0200

    NEW

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 window.addEventListener('load', function () {
-    const elem = document.querySelector('.container');
-    const imageElem = document.querySelector('.image');
+    const  elem = document.querySelector('section.container');
+    const  imageElem = document.querySelectorAll('img');
     
     elem.classList.add('containerActive');
+
     imageElem.addEventListener('mousemove', function (event) {
-        const xPosition= event.layerX;
+        const xPosition= event.layerX;  
         const yPosition= event.layerY;
 
         imageElem.style.transform = `rotateY(${xPosition/50}deg) rotateX(${-yPosition/50}deg)`;
