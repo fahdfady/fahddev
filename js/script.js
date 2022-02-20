16a3a7853675f96ca7f29e5619de4c3859b03bf5
fahdfady <55513802+fahdfady@users.noreply.github.com>
2022-02-20 03:42:57 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
+// loader
+let body = document.querySelector('body');
+
 window.addEventListener('load', function () {
-    const  elem = document.querySelector('section.container');
-    const  imageElem = document.querySelectorAll('img');
-    
-    elem.classList.add('containerActive');
-
-    imageElem.addEventListener('mousemove', function (event) {
-        const xPosition= event.layerX;  
-        const yPosition= event.layerY;
-
-        imageElem.style.transform = `rotateY(${xPosition/50}deg) rotateX(${-yPosition/50}deg)`;
-    });
+    setTimeout(function () { body.classList.add("loaded") }, 5000);
 });
+
+
+// cursor
+let mouseCursor = document.querySelector('.cursor');
+let mouseDot = document.querySelector('.dot');
+let smallSpeed = 0.2;
+let largeSpeed = 0.12;
+
+
+window.addEventListener('mousemove', moveCursor);
+
+
+function moveCursor(e) {
+    let placeX = e.pageX - 25;
+    let placeY = e.pageY - 25;
+    let placeXS = e.pageX - 4;
+    let placeYS = e.pageY - 4;
+
+    mouseCursor.style.transform = "translate(" + placeX + 'px,' + placeY + 'px)';
+
+    mouseDot.style.transform = "translate(" + placeXS + 'px,' + placeYS + 'px)';
+
+}
+
+let header = document.querySelector('header.page-header'),
+
+    nav = document.querySelector('header.page-header nav.navbar');
+header.style.paddingBottom = nav.offsetHeight;
+
+let notFooter = document.querySelector('.not-footer');
+let foooter = document.querySelector("footer.page-footer");
+
+notFooter.style.paddingBottom = foooter.offsetHeight;
