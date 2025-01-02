19ea2dd8c213077979f51b88f65af816531453d9
fahdfady <fahd.fady212@gmail.com>
2025-01-02 22:53:30 +0200

    spiderman responsive

--- src/app/components/SpidermanGIF.tsx
+++ src/app/components/SpidermanGIF.tsx
@@ -1,7 +1,7 @@
 export default function SpidermanGIF() {
     return (
         <>
-            <iframe src="https://giphy.com/embed/1xnOVciG4m6DDrS90I" width="480" height="343" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/leroypatterson-animated-1xnOVciG4m6DDrS90I">via GIPHY</a></p>
+            <iframe src="https://giphy.com/embed/1xnOVciG4m6DDrS90I" width="480" height="343" frameBorder="0" className="giphy-embed max-w-full" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/leroypatterson-animated-1xnOVciG4m6DDrS90I">via GIPHY</a></p>
         </>
     )
 }
