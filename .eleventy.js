97386aff167fb8880008feb9443905f96fe85e48
Fahd Ashour <fahd.fady212@gmail.com>
2026-06-17 11:26:14 +0300

    add ability to post external blogs i posted somewhere else. add bleu and blended blogs. make blogs visible on homepage.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function (eleventyConfig) {
+    const externalBlogs = require("./src/_data/externalBlogs.json");
+
     // Copy static assets
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("public");
@@ -20,6 +22,45 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addCollection("posts", function (collectionApi) {
         return collectionApi.getFilteredByGlob("src/blog/*.md").sort((a, b) => {
             return new Date(b.data.date) - new Date(a.data.date);
+        });
+    });
+
+    eleventyConfig.addCollection("allBlogPosts", function (collectionApi) {
+        const localPosts = collectionApi.getFilteredByGlob("src/blog/*.md").map((post) => ({
+            url: post.url,
+            data: {
+                ...post.data,
+                external: false
+            }
+        }));
+
+        const externalPosts = externalBlogs.map((post, index) => ({
+            url: post.url,
+            data: {
+                ...post,
+                external: true,
+                tags: post.tags || [],
+                sortOrder: post.sortOrder ?? index
+            }
+        }));
+
+        return [...localPosts, ...externalPosts].sort((a, b) => {
+            const dateA = a.data.date ? new Date(a.data.date) : null;
+            const dateB = b.data.date ? new Date(b.data.date) : null;
+
+            if (dateA && dateB) {
+                return dateB - dateA;
+            }
+
+            if (dateA) {
+                return -1;
+            }
+
+            if (dateB) {
+                return 1;
+            }
+
+            return a.data.sortOrder - b.data.sortOrder;
         });
     });
 
